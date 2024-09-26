5f95c97f058a852a00558b35b8fd3ad3e991d570
Isaac Rowell <isaacgrowell@gmail.com>
2024-09-26 04:54:26 -0400

    Add some additional config options

---
+++ README.md
@@ -0,0 +1,10 @@
+# Firefly-III Auto-Savings
+
+## A script to automate autosavings transfers based on widthdrawls
+
+Some banks offer 'simply savings' where some amount ($0.50, $1 etc.) is
+deposited into your savings account for every transaction in your chequing
+account.
+
+This is annoying to enter everytime, so this script serves as a way to auomate
+this.
--- firefly_3_auto_savings.py
+++ firefly_3_auto_savings.py
@@ -1,36 +1,63 @@
-import json
-import sys
-import os
 import argparse
 from datetime import datetime, timedelta
 import requests
 
-URL = 'http://localhost:8100/api/v1/transactions'
+BASE_URL = '/api/v1'
+TRANSACTIONS_URL = '/transactions'
 HEADERS = {
     'Accept': 'application/vnd.api+json'
 }
 
-VALID_TRANSACTION_TYPES=['withdrawal', 'withdrawals'] # which transaction types trigger a transfer
-IGNORE_CATEGORIES = ['Insurance', 'Rent'] # skip these categories if a withdrawal is made to them
+# which transaction types trigger a transfer
+VALID_TRANSACTION_TYPES = ['withdrawal', 'withdrawals']
+
+# skip these categories if a withdrawal is made to them
+IGNORE_CATEGORIES = ['Bills']
 
 AUTOSAVE_DESCRIPTION = '(auto-savings transfer)'
 AUTOSAVE_CATEGORY = '(auto-savings)'
 
-CASH_ACCOUNT_NAME='Cash wallet' # name of the account you 'transfer' to when 'withdrawing' cash irl (i.e. from an ATM)
+# name of the account you 'transfer' to when 'withdrawing' cash irl (i.e. from an ATM)
+CASH_ACCOUNT_NAME = 'Cash wallet'
 
-TRANSFER_CURRENCY='CAD'
+TRANSFER_CURRENCY = 'CAD'
 
 
-def get_all_transactions(page=1):
-    response = requests.get(URL+'?page=%s' % page, headers=HEADERS)
+def _get_target(ip: str, port: str, proto: str = 'https') -> str:
+    host = f'{proto}://{ip}:{port}'
+    path = f'{BASE_URL}' + f'{TRANSACTIONS_URL}'
+    return host + path
+
+
+def get_all_transactions(
+    target: str,
+    page: int = 1,
+):
+    response = requests.get(
+        target+'?page=%s' % page,
+        headers=HEADERS,
+        verify=False,
+        timeout=30
+    )
     response = response.json()
-    if response['meta']['pagination']['current_page'] == response['meta']['pagination']['total_pages']:
+    current_page = response['meta']['pagination']['current_page']
+    total_pages = response['meta']['pagination']['total_pages']
+    if current_page == total_pages:
         return response['data']
-    response['data'].extend(get_all_transactions(page=response['meta']['pagination']['current_page']+1))
+    response['data'].extend(
+        get_all_transactions(
+            target,
+            page=current_page+1
+        )
+    )
     return response['data']
 
 
-def filter_valid_transactions(source_acct, transactions, include_cash_transfer=False):
+def filter_valid_transactions(
+    source_acct,
+    transactions,
+    include_cash_transfer=False,
+):
     valid_transactions = []
     for transaction in transactions:
         for transaction_split in transaction['attributes']['transactions']:
@@ -39,31 +66,39 @@ def filter_valid_transactions(source_acct, transactions, include_cash_transfer=F
                     continue
                 if transaction_split['type'] in VALID_TRANSACTION_TYPES:
                     valid_transactions.append({
-                            'currency_code': transaction_split['currency_code'],
-                            'amount': transaction_split['amount'],
-                            'description': transaction_split['description'],
-                            'date': transaction_split['date']
-                        })
+                        'currency_code': transaction_split['currency_code'],
+                        'amount': transaction_split['amount'],
+                        'description': transaction_split['description'],
+                        'date': transaction_split['date'],
+                    })
                 elif include_cash_transfer and transaction_split['type'] == 'transfer' \
-                    and transaction_split['destination_name'] == CASH_ACCOUNT_NAME:
+                        and transaction_split['destination_name'] == CASH_ACCOUNT_NAME:
                     valid_transactions.append({
-                            'currency_code': transaction_split['currency_code'],
-                            'amount': transaction_split['amount'],
-                            'description': transaction_split['description'],
-                            'date': transaction_split['date']
-                        })
+                        'currency_code': transaction_split['currency_code'],
+                        'amount': transaction_split['amount'],
+                        'description': transaction_split['description'],
+                        'date': transaction_split['date']
+                    })
 
-    return valid_transactions 
+    return valid_transactions
 
 
-def create_auto_savings_transactions(valid_transactions, source_acct, dest_acct, amount, apply=False):
+def create_auto_savings_transactions(
+    valid_transactions,
+    source_acct,
+    dest_acct,
+    amount,
+    apply=False,
+):
     # for each transaction on a given date, create a new transfer
     # of the 'amount' specified for the next business day
-    current_date = datetime.now()
     date_to_transactions = {}
 
     for transaction in valid_transactions:
-        transaction_date = datetime.strptime(transaction['date'].split('T')[0], '%Y-%m-%d')
+        transaction_date = datetime.strptime(
+            transaction['date'].split('T')[0],
+            '%Y-%m-%d',
+        )
 
         # if the transaction is on a weekend, it wont get processed until Monday
         # and so the auto save wont go out until a day after that
@@ -80,47 +115,105 @@ def create_auto_savings_transactions(valid_transactions, source_acct, dest_acct,
 
     for date, transactions in date_to_transactions.items():
         total = amount * len(transactions)
-        print('Creating auto-save transfer for {date}. Total {total}. Includes {num_t} transactions. Source {source}. Dest {dest}. '\
-                .format(date=date, source=source_acct, dest=dest_acct, num_t=len(transactions), total=total))
-        
+        print(
+            f'Creating auto-save transfer for {date}. '
+            f'Total {total}. Includes {len(transactions)} transactions. '
+            f'Source {source_acct}. '
+            f'Dest {dest_acct}. '
+        )
+
         if apply:
             payload = {
-                    'error_if_duplicate_hash': False,
-                    'transactions': [
-                        {
-                            'type': 'transfer', 
-                            'date': date.strftime('%Y-%m-%dT%H:%M:%S%Z'), 
-                            'amount': total, 
-                            'description': AUTOSAVE_DESCRIPTION, 
-                            'category_name': AUTOSAVE_CATEGORY,
-                            'source_name': source_acct,
-                            'destination_name': dest_acct
-                            }
-                        ]
+                'error_if_duplicate_hash': False,
+                'transactions': [
+                    {
+                        'type': 'transfer',
+                        'date': date.strftime('%Y-%m-%dT%H:%M:%S%Z'),
+                        'amount': total,
+                        'description': AUTOSAVE_DESCRIPTION,
+                        'category_name': AUTOSAVE_CATEGORY,
+                        'source_name': source_acct,
+                        'destination_name': dest_acct
                     }
+                ]
+            }
             response = requests.post(URL, headers=HEADERS, json=payload)
             print(response.text)
 
 
 def parse_commandline():
     parser = argparse.ArgumentParser(
-        description='Create an (auto-savings transfer) transaction of the specified amount for each widthdrawal \
+        description='Create an (auto-savings transfer) transaction of the \
+                        specified amount for each widthdrawal \
                         from source account to destination account. \
                         This would mock something like the "simply savings" transfer that the \
-                        banks can setup everytime your debit card is used.', 
+                        banks can setup everytime your debit card is used.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    parser.add_argument('transfer_amount', type=float, help='The amount to transfer from the source account to the destination account')
-    parser.add_argument('source_account', help='The name of the source account to transfer from')
-    parser.add_argument('destination_account', help='The name of the destination account to transfer to')
-    parser.add_argument('--token', default='.firefly_api_token', help='Location of API token')
-    parser.add_argument('--ignore-categories', nargs='+', default=IGNORE_CATEGORIES, help='Categories to ignore, i.e. "Credit Card"')
-    parser.add_argument('--include-cash-transfer', action='store_true', default=False, 
-            help='Some banks include widthdrawing cash as part of the simply savings transactions.\
-                    If you have firefly setup to "transfer" into a cash wallet, this can get missed since its not a withdrawal.')
-    parser.add_argument('--apply', action='store_true', default=False, help='Apply the transactions to the destination account, otherwise just print what it will do.')
-    parser.add_argument('--since-date', help='Only calculate transactions since a certain date')
-    parser.add_argument('--until-date', help='Only calculate transactions until a certain date')
+    parser.add_argument(
+        'transfer_amount',
+        type=float,
+        help='The amount to transfer from the source account \
+                to the destination account',
+    )
+    parser.add_argument(
+        'source_account',
+        help='The name of the source account to transfer from',
+    )
+    parser.add_argument(
+        'destination_account',
+        help='The name of the destination account to transfer to',
+    )
+    parser.add_argument(
+        '--ip',
+        default='127.0.0.1',
+        help='IP of the firefly server',
+    )
+    parser.add_argument(
+        '--port',
+        default='443',
+        help='HTTP port of the firefly server',
+    )
+    parser.add_argument(
+        '--proto',
+        default='https',
+        help='http or https',
+    )
+    parser.add_argument(
+        '--token',
+        default='.firefly_api_token',
+        help='Location of API token',
+    )
+    parser.add_argument(
+        '--ignore-categories',
+        nargs='+',
+        default=IGNORE_CATEGORIES,
+        help='Categories to ignore, i.e. "Credit Card"',
+    )
+    parser.add_argument(
+        '--include-cash-transfer',
+        action='store_true',
+        default=False,
+        help='Some banks include widthdrawing cash as part of the \
+            simply savings transactions.\
+            If you have firefly setup to "transfer" into a cash wallet, \
+            this can get missed since its not a withdrawal.'
+    )
+    parser.add_argument(
+        '--apply',
+        action='store_true',
+        default=False,
+        help='Apply the transactions to the destination account, \
+                otherwise just print what it will do.',
+    )
+    parser.add_argument(
+        '--since-date',
+        help='Only calculate transactions since a certain date',
+    )
+    parser.add_argument(
+        '--until-date',
+        help='Only calculate transactions until a certain date',
+    )
 
     return parser.parse_args()
 
@@ -130,17 +223,30 @@ def main():
     source_acct = options.source_account
     dest_acct = options.destination_account
     amount = options.transfer_amount
-    IGNORE_CATEGORIES.concat(options.ignore_categories)
+    IGNORE_CATEGORIES.extend(options.ignore_categories)
 
     with open(options.token) as f:
         token = f.read().rstrip()
 
     HEADERS['Authorization'] = 'Bearer {}'.format(token)
 
-    transactions = get_all_transactions()
-    valid_transactions = filter_valid_transactions(source_acct, transactions, include_cash_transfer=options.include_cash_transfer)
+    target = _get_target(options.ip, options.port, options.proto)
+    transactions = get_all_transactions(
+        target
+    )
+    valid_transactions = filter_valid_transactions(
+        source_acct,
+        transactions,
+        include_cash_transfer=options.include_cash_transfer,
+    )
 
-    create_auto_savings_transactions(valid_transactions, source_acct, dest_acct, amount, apply=options.apply)
+    create_auto_savings_transactions(
+        valid_transactions,
+        source_acct,
+        dest_acct,
+        amount,
+        apply=options.apply,
+    )
 
 
 if __name__ == '__main__':
